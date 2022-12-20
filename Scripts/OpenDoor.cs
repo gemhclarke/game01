8fdaf407011c6ad841b315f30da79cc376c8cb98
Bob Clarke <bobmclarke@gmail.com>
2022-12-20 23:25:29 +0000

    Added Box collider to RedKeySquare, changed Collision script for player from if statement to a case statement (more efficent)

--- Scripts/OpenDoor.cs
+++ Scripts/OpenDoor.cs
@@ -7,13 +7,6 @@ public class OpenDoor : MonoBehaviour
     public Animation hinge;
     public Component doorcollider;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
     void OnTriggerStay()
     {
         if(Input.GetKey(KeyCode.E))
--- Scripts/collision.cs
+++ Scripts/collision.cs
@@ -7,9 +7,24 @@ public class collision : MonoBehaviour
 {
     void OnTriggerEnter(Collider other)
     {
+            /*
             if (other.gameObject.tag == "Patroller")
             {
                 SceneManager.LoadScene("Game");
             }
+            */
+
+            switch (other.gameObject.tag) 
+            {
+            case "Patroller":
+                Debug.Log("Collided with Patroller");
+                SceneManager.LoadScene("Game");
+                break;
+            case "Redkey":
+                Debug.Log("Collided with Redkey");
+                break;
+            }
+
+
     }
 }
